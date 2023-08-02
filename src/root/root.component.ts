506938f05d64e8f38468d382f07de3d8ceb06fef
Saranraj-SR <89800236+Saranraj-SR@users.noreply.github.com>
2023-08-02 10:10:19 +0530

    3 files modified

--- src/root/root.component.ts
+++ src/root/root.component.ts
@@ -11,79 +11,38 @@ import { AppSettings } from './root.settings';
   styleUrls: ['./root.component.css'],
   standalone: true,
   imports: [RootModule],
-  animations: [
-    trigger('sidebar', [
-      state(
-        'true',
-        style({
-          width: '250px',
-        })
-      ),
-      state(
-        'false',
-        style({
-          width: '0',
-        })
-      ),
-    ]),
-    trigger('content', [
-      state(
-        'true',
-        style({
-          width: 'calc(100% - 250px)',
-        })
-      ),
-      state(
-        'false',
-        style({
-          width: '100%',
-        })
-      ),
-    ]),
-  ],
+  // animations: [
+  //   trigger('sidebar', [
+  //     state(
+  //       'true',
+  //       style({
+  //         width: '250px',
+  //       })
+  //     ),
+  //     state(
+  //       'false',
+  //       style({
+  //         width: '0',
+  //       })
+  //     ),
+  //   ]),
+  //   trigger('content', [
+  //     state(
+  //       'true',
+  //       style({
+  //         width: 'calc(100% - 250px)',
+  //       })
+  //     ),
+  //     state(
+  //       'false',
+  //       style({
+  //         width: '100%',
+  //       })
+  //     ),
+  //   ]),
+  // ],
 })
 export class RootComponent implements OnInit {
-  public settings: Settings;
   ngOnInit() {}
   title = '';
-  constructor(private router: Router, private appSettings: AppSettings) {
-    this.settings = this.appSettings.settings;
-    this.title = this.settings.name ?? '';
-    this.settings.sidenavIsOpened = false;
-
-    // Close the dropdown if the user clicks outside of it
-    window.onclick = function (event: any) {
-      if (!event.target.matches('.dropbtn')) {
-        var dropdowns = document.getElementsByClassName('dropdown-content');
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-          var openDropdown = dropdowns[i];
-          if (openDropdown.classList.contains('show')) {
-            openDropdown.classList.remove('show');
-          }
-        }
-      }
-    };
-  }
-
-  toggleMenu() {
-    this.settings.sidenavIsOpened = !this.settings.sidenavIsOpened;
-  }
-
-  @HostListener('window:resize')
-  public onWindowResize(): void {
-    if (window.innerWidth <= 768) {
-      this.settings.sidenavIsOpened = false;
-      this.settings.sidenavIsPinned = false;
-    } else {
-      this.settings.sidenavIsOpened = true;
-      this.settings.sidenavIsPinned = true;
-    }
-  }
-
-  myFunction() {
-    (<HTMLDivElement>document.getElementById('myDropdown')).classList.toggle(
-      'show'
-    );
-  }
 }
